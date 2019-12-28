961b1231a3437c64936df8bdb5d2ff355671200d
Ge <55292128+alpharout@users.noreply.github.com>
2019-12-28 18:01:42 +0100

    Added Join Event

--- src/de/craftagain/lobbysystem/main/LobbySystem.java
+++ src/de/craftagain/lobbysystem/main/LobbySystem.java
@@ -1,13 +1,20 @@
 package de.craftagain.lobbysystem.main;
 
 import de.alpharout.lobbysystem.data.Data;
+import de.alpharout.lobbysystem.events.PlayerJoinListener;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LobbySystem extends JavaPlugin {
+    PluginManager pluginManager;
 
     @Override
     public void onEnable() {
         Data.log("§7Lobby System started, coded by " + Data.getAuthors() + "!");
+
+        pluginManager = Bukkit.getPluginManager();
+        pluginManager.registerEvents(new PlayerJoinListener(), this);
     }
 
     @Override
