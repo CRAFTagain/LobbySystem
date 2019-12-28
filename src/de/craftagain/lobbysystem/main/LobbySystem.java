7ee7794d392d6055d61babb13df1f14fab0dec23
Ge <55292128+alpharout@users.noreply.github.com>
2019-12-28 18:11:10 +0100

    Registered Join Listener

--- src/de/craftagain/lobbysystem/main/LobbySystem.java
+++ src/de/craftagain/lobbysystem/main/LobbySystem.java
@@ -1,14 +1,22 @@
 package de.craftagain.lobbysystem.main;
 
 import de.alpharout.lobbysystem.data.Data;
+import de.alpharout.lobbysystem.events.PlayerJoinListener;
 import de.craftagain.lobbysystem.command.CMD_info;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LobbySystem extends JavaPlugin {
+    PluginManager pluginManager;
 
     @Override
     public void onEnable() {
         Data.log("§7Lobby System started, coded by " + Data.getAuthors() + "!");
+
+        pluginManager = Bukkit.getPluginManager();
+
+        pluginManager.registerEvents(new PlayerJoinListener(), this);
 
         getCommand("info").setExecutor(new CMD_info());
     }
